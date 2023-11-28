16fb85f80ac3e365a2f557fd56352fbcc530d4bb
mohamed <mohamed@gmail.com>
2023-11-28 11:46:00 +0200

    add some of changes

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for i in range(8):
     for j in range(10):
         if j > i and j != i:
-            print("{:d}{:d}".format(i, j), end= ", ")
+            print("{:d}{:d}".format(i, j), end=", ")
 print(89)

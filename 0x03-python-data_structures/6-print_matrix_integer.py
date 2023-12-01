6224dd29a8d524e1baf8cee27442b9915c713888
mohamed <mohamed@gmail.com>
2023-12-01 23:27:36 +0200

    add some files

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for lisst in matrix:
+        l = (len(lisst) - 1)
         for num in lisst:
-            print("{:d}".format(num), end=" ") 
+            if l == 0:
+                print("{:d}".format(num), end="")
+            else:
+                print("{:d}".format(num), end=" ")
+            l -= 1
+        print()
+

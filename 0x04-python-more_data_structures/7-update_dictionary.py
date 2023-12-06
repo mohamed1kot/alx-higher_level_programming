139180e3431799b839afbf47cea0e23fdac4b3ff
mohamed <mohamed@gmail.com>
2023-12-06 21:51:34 +0200

    add changes

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary:
-        a_dictionary[key] = value
-    else:
-        a_dictionary[kew] = value
+    a_dictionary[key] = value
     return a_dictionary
 

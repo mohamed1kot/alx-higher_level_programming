8acd26e167ae1c0a710df2225dece2a8b62901fb
mohamed <mohamed@gmail.com>
2023-12-06 17:12:23 +0200

    add another one file

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,5 +4,6 @@ def search_replace(my_list, search, replace):
     for num in my_list:
         if num == search:
             new_list.append(replace)
-        new_list.append(num)
+        else:
+            new_list.append(num)
     return (new_list)

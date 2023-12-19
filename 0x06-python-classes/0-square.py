1dea75af75f8cf4c11217c823e96feb6adef16cd
mohamed <mohamed@gmail.com>
2023-12-19 22:42:47 +0200

    add another one file

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""this class Square is empty class"""
+"""This class Square is empty class"""
 
 class Square:
-    """this class Square is empty class"""
+    """This class Square is empty class"""
     pass

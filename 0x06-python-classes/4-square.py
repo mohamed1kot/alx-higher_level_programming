504690b58756753883506703d1c60aa3e2829bb6
mohamed <mohamed@gmail.com>
2023-12-20 08:31:48 +0200

    add another one file

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""class Square that defines a square by: (based on 3-square.py)"""
+
+
+class Square:
+    """
+    a class Square that defines a square by: (based on 3-square.py)
+
+    Attributes:
+        size: Private instance attribute
+    """
+    def __init__(self, size):
+        """
+        new instance form class
+
+        Args:
+            size: size of the square
+        """
+        self.__size = size
+
+    def area(self):
+        """
+        public instance method that returns the current square area
+
+        args:
+            size: size of the square
+        """
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """
+        return the size of the square
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+        size must be an integer, otherwise raise a TypeError
+        exception with the message size must be an integer
+
+        if size is less than 0, raise a ValueError
+        exception with the message size must be >= 0
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value

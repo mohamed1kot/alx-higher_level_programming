9f3c2f7d0eae08d8432bc44f6537bb794b7c414a
mohamed <mohamed@gmail.com>
2024-01-09 23:12:53 +0200

    add another fiel

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" a function that returns an object represented by a JSON string"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    a function that returns an object represented by a JSON string.
+
+    Args:
+        my_str: the str will be converted to dict.
+    Return:
+        an object represented by a JSON string.
+    """
+    return (json.loads(my_str))

f2acfe26c520a1fa1b7abcf2332fbe5ae8c73255
mohamed <mohamed@gmail.com>
2024-01-09 23:06:59 +0200

    add another task

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,5 +5,12 @@ import json
 
 def to_json_string(my_obj):
     """
+    a function that returns the JSON representation of an object (string)
+
+    Args:
+        my_obj: josn file
+
+    Return:
+        the JSON representation of an object (string).
     """
     return (json.dumps(my_obj))

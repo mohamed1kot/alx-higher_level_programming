9496a85056a6b8a399d0671d52288da692489900
mohamed <mohamed@gmail.com>
2024-01-10 20:08:25 +0200

    add another new task

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""a class BaseGeometry (based on 5-base_geometry.py)."""
+
+
+class BaseGeometry:
+    """
+    a class BaseGeometry (based on 5-base_geometry.py).
+    """
+    def area(self):
+        """
+        instance method: def area(self): that raises an
+        Exception with the message area() is not implemented
+        """
+        raise Exception("area() is not implemented")
